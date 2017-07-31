a9439fc143e1bdd2534f1e9584123da557751f6a
dsaves <dronin227@gmail.com>
2017-07-31 09:28:55 -0600

    Added line to import aes-256 package.

--- crypto/aes_256/aes_256_core.vhdl
+++ crypto/aes_256/aes_256_core.vhdl
@@ -1,6 +1,35 @@
+--MIT License
+--
+--Copyright (c) 2017  Danny Savory
+--
+--Permission is hereby granted, free of charge, to any person obtaining a copy
+--of this software and associated documentation files (the "Software"), to deal
+--in the Software without restriction, including without limitation the rights
+--to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+--copies of the Software, and to permit persons to whom the Software is
+--furnished to do so, subject to the following conditions:
+--
+--The above copyright notice and this permission notice shall be included in all
+--copies or substantial portions of the Software.
+--
+--THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+--IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+--FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+--AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+--LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+--OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+--SOFTWARE.
+
+
+-- ############################################################################
+--  The official specifications of the SHA-256 algorithm can be found here:
+--      http://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.180-4.pdf
+
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use work.aes_256_pkg.all;
 
 entity aes_256_core is
     generic(
@@ -17,6 +46,7 @@ end entity;
 architecture aes_256_core_ARCH of aes_256_core is
     constant KEY_SIZE : natural := 256; -- AES-256 has a 256-bit key size
 begin
+
     --TODO: fix dummy output
     data_out <= (others => '0');
     
